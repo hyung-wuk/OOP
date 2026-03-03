844305f314ae98fd0c46816611ec40bcd381d043
hyung-wuk <2wook040809@gmail.com>
2026-03-03 20:11:12 +0900

    26.03.02-study-commit

---
+++ 2026.03.02/src/org/opentutorials/javatutorials/array/GetDemo.java
@@ -0,0 +1,12 @@
+package org.opentutorials.javatutorials.array;
+
+public class GetDemo {
+    public static void main(String[] args) {
+
+        String[] calssGroup = {"최진혁", "최유빈", "한이람", "이고잉"};
+        System.out.println(calssGroup[0]);
+        System.out.println(calssGroup[1]);
+        System.out.println(calssGroup[2]);
+        System.out.println(calssGroup[3]);
+    }
+}
