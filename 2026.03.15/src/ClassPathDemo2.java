7995d6e049b48e2fee8e238762ebff18a9e3c375
hyung-wuk <2wook040809@gmail.com>
2026-03-23 11:21:35 +0900

    2026.03.15-study-commit

---
+++ 2026.03.15/src/ClassPathDemo2.java
@@ -0,0 +1,11 @@
+public class ClassPathDemo2 {
+    public static void main(String[] args) {
+        Item2 i1 = new Item2();
+        i1.print();
+    }
+}
+class Item2{
+    public void print(){
+        System.out.println("Hello world");
+    }
+}
